094b6ae03c66c01b75b9681ab57e9f9fac58e4d6
Marcus Gruneau <marcus.gruneau@gmail.com>
2017-08-29 17:14:25 +0200

    fix tests

--- src/test/java/se/gruneau/utils/TemperatureUtilTest.java
+++ src/test/java/se/gruneau/utils/TemperatureUtilTest.java
@@ -1,24 +1,28 @@
 package se.gruneau.utils;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by gruneau on 2017-08-29.
  */
-public class TemperatureUtilTest extends TestCase {
+public class TemperatureUtilTest {
 
     private final TemperatureUtil temperatureUtil = new TemperatureUtil();
 
+    @Test
     public void testToCelsius() throws Exception {
         final Double fahrenheit = 32.0;
         final Double result = temperatureUtil.toCelsius(fahrenheit);
-        assertEquals(0.0, result);
+        assertEquals(0.0, result, 0);
     }
 
+    @Test
     public void testToCelsius2() throws Exception {
         final Double fahrenheit = -18.23;
         final Double result = temperatureUtil.toCelsius(fahrenheit);
-        assertEquals(-27.90555555555556, result);
+        assertEquals(-27.90555555555556, result, 0);
     }
 
 }
