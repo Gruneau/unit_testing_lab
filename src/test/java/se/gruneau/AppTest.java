0e8a2658f88cbb8457a435dd70353ef2170ee875
Marcus Gruneau <marcus.gruneau@gmail.com>
2017-08-29 16:14:57 +0200

    init repo

---
+++ src/test/java/se/gruneau/AppTest.java
@@ -0,0 +1,19 @@
+package se.gruneau;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
