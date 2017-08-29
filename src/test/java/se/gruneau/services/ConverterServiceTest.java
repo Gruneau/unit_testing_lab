825ad2fb8de0ea98ed43de53c18dc524f5f5ab28
Marcus Gruneau <marcus.gruneau@gmail.com>
2017-08-29 17:33:22 +0200

    add list test

---
+++ src/test/java/se/gruneau/services/ConverterServiceTest.java
@@ -0,0 +1,44 @@
+package se.gruneau.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+import se.gruneau.utils.TemperatureUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by gruneau on 2017-08-29.
+ */
+public class ConverterServiceTest {
+
+    private final TemperatureUtil temperatureUtil = mock(TemperatureUtil.class);
+
+    private final ConverterService converterService = new ConverterService(temperatureUtil);
+
+    @Test
+    public void testConvertToCelsius() throws Exception {
+
+        //Arrange
+        List<Double> fahrenheit = new ArrayList<>();
+        fahrenheit.add(12.0);
+        fahrenheit.add(-25.12);
+        fahrenheit.add(0.0);
+
+        //Hint: At this point we can mock up the temperature util completely! (We already covered it in its own test class.)
+        //In larger and more complex projects, this can turn an insanely huge test redundant - stick to small and meaningful tests,
+        //Large overlapping tests are complex to understand and maintain, try to mock away everything that is not relevant for the test.
+        when(temperatureUtil.toCelsius(any(Double.class))).thenReturn(any(Double.class));
+
+        //Act
+        List<Double> results = converterService.convertToCelsius(fahrenheit);
+
+        //Assert
+        Assert.assertEquals(3, results.size());
+    }
+
+}
